c649a5ebb2e15e017c48b2f10454444d34342051
Doaa Mahely <dom.k9@hotmail.com>
2018-07-25 18:02:57 +0400

    url_for error debugging

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -19,50 +19,56 @@ def show_restaurants():
     return render_template('restaurants.html', restaurants = restaurants)
 
 # adding new restaurant
-@app.route('/restaurant/new/')
+@app.route('/restaurant/new/', methods = ['GET', 'POST'])
 def new_restaurant():
-    return render_template('newRestaurant.html')
+    if request.method == 'POST':
+        new_entry = Restaurant(name=request.form.input['name'])
+        session.add(new_entry)
+        session.commit()
+        return redirect(url_for('show_restaurants'))
+    if request.method == 'GET':
+        return render_template('newRestaurant.html')
 
-# editing existing restaurant
-@app.route('/restaurant/<int:restaurant_id>/edit/')
-def edit_restaurant(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    return render_template('editRestaurant.html', restaurant = restaurant)
-
-# deleting restaurant
-@app.route('/restaurant/<int:restaurant_id>/delete/')
-def delete_restaurant(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    return render_template('deleteRestaurant.html', restaurant = restaurant)
-
-# showing menu of a restaurant
-@app.route('/restaurant/<int:restaurant_id>/')
-@app.route('/restaurant/<int:restaurant_id>/menu/')
-def show_menu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
-    return render_template('menu.html', restaurant = restaurant, items = menu_items)
-
-# adding new menu item
-@app.route('/restaurant/<int:restaurant_id>/menu/new/')
-def new_menu_item(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    return render_template('newMenuItem.html', restaurant = restaurant)
-
-# editing existing menu item
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
-def edit_menu_item(restaurant_id, menu_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    menu_item = session.query(MenuItem).filter_by(id = menu_id).one()
-    return render_template('editMenuItem.html', restaurant = restaurant, item = menu_item)
-
-# deleting menu item
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
-def delete_menu_item(restaurant_id, menu_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    menu_item = session.query(MenuItem).filter_by(id = menu_id).one()
-    return render_template('deleteMenuItem.html', restaurant = restaurant, item = menu_item)
-
+### editing existing restaurant
+##@app.route('/restaurant/<int:restaurant_id>/edit/')
+##def edit_restaurant(restaurant_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    return render_template('editRestaurant.html', restaurant = restaurant)
+##
+### deleting restaurant
+##@app.route('/restaurant/<int:restaurant_id>/delete/')
+##def delete_restaurant(restaurant_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    return render_template('deleteRestaurant.html', restaurant = restaurant)
+##
+### showing menu of a restaurant
+##@app.route('/restaurant/<int:restaurant_id>/')
+##@app.route('/restaurant/<int:restaurant_id>/menu/')
+##def show_menu(restaurant_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    menu_items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+##    return render_template('menu.html', restaurant = restaurant, items = menu_items)
+##
+### adding new menu item
+##@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+##def new_menu_item(restaurant_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    return render_template('newMenuItem.html', restaurant = restaurant)
+##
+### editing existing menu item
+##@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+##def edit_menu_item(restaurant_id, menu_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    menu_item = session.query(MenuItem).filter_by(id = menu_id).one()
+##    return render_template('editMenuItem.html', restaurant = restaurant, item = menu_item)
+##
+### deleting menu item
+##@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+##def delete_menu_item(restaurant_id, menu_id):
+##    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+##    menu_item = session.query(MenuItem).filter_by(id = menu_id).one()
+##    return render_template('deleteMenuItem.html', restaurant = restaurant, item = menu_item)
+##
 
 # will only run from python interpretor
 if __name__ == '__main__':
