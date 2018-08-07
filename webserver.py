aae904fff2ec60ce08d5054d28a2d91752ea766c
Doaa Mahely <dom.k9@hotmail.com>
2018-08-08 00:57:16 +0400

    Added google callback method and enabled connection to google server

--- webserver.py
+++ webserver.py
@@ -1,12 +1,17 @@
-# Client id: 413629628721-4g3mhr1iko0jv5nmg1tsiqgcqn24g1nt.apps.googleusercontent.com
-# Client secret: caYnfx6fj2U3zQnaxuACLJUS
 from flask import Flask, render_template, url_for, redirect, request, jsonify, flash
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 # imports for OAuth
-from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 import random, string
+from flask import session as login_session
 
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
@@ -15,6 +20,204 @@ engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = 'Restaurant Menu Web Application'
+##
+### Create anti-forgery state token
+##@app.route('/login/')
+##def showLogin():
+##    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+##                    for x in xrange(32))
+##    login_session['state'] = state
+##    # return "The current session state is %s" % login_session['state']
+##    return render_template('login.html', STATE=state)
+##
+##
+##@app.route('/gconnect', methods=['POST'])
+##def gconnect():
+##    # Validate state token
+##    if request.args.get('state') != login_session['state']:
+##        response = make_response(json.dumps('Invalid state parameter.'), 401)
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##    # Obtain authorization code
+##    code = request.data
+##
+##    try:
+##        # Upgrade the authorization code into a credentials object
+##        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+##        oauth_flow.redirect_uri = 'postmessage'
+##        credentials = oauth_flow.step2_exchange(code)
+##    except FlowExchangeError:
+##        response = make_response(
+##            json.dumps('Failed to upgrade the authorization code.'), 401)
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##
+##    # Check that the access token is valid.
+##    access_token = credentials.access_token
+##    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+##           % access_token)
+##    h = httplib2.Http()
+##    result = json.loads(h.request(url, 'GET')[1])
+##    # If there was an error in the access token info, abort.
+##    if result.get('error') is not None:
+##        response = make_response(json.dumps(result.get('error')), 500)
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##
+##    # Verify that the access token is used for the intended user.
+##    gplus_id = credentials.id_token['sub']
+##    if result['user_id'] != gplus_id:
+##        response = make_response(
+##            json.dumps("Token's user ID doesn't match given user ID."), 401)
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##
+##    # Verify that the access token is valid for this app.
+##    if result['issued_to'] != CLIENT_ID:
+##        response = make_response(
+##            json.dumps("Token's client ID does not match app's."), 401)
+##        print "Token's client ID does not match app's."
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##
+##    stored_access_token = login_session.get('access_token')
+##    stored_gplus_id = login_session.get('gplus_id')
+##    if stored_access_token is not None and gplus_id == stored_gplus_id:
+##        response = make_response(json.dumps('Current user is already connected.'),
+##                                 200)
+##        response.headers['Content-Type'] = 'application/json'
+##        return response
+##
+##    # Store the access token in the session for later use.
+##    login_session['access_token'] = credentials.access_token
+##    login_session['gplus_id'] = gplus_id
+##
+##    # Get user info
+##    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+##    params = {'access_token': credentials.access_token, 'alt': 'json'}
+##    answer = requests.get(userinfo_url, params=params)
+##
+##    data = answer.json()
+##
+##    login_session['username'] = data['name']
+##    login_session['picture'] = data['picture']
+##    login_session['email'] = data['email']
+##
+##    output = ''
+##    output += '<h1>Welcome, '
+##    output += login_session['username']
+##    output += '!</h1>'
+##    output += '<img src="'
+##    output += login_session['picture']
+##    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+##    flash("you are now logged in as %s" % login_session['username'])
+##    print "done!"
+##    return output
+
+
+# login page
+# will create a state token to prevent request forgery
+# to be stored in the session for later validation
+@app.route('/login/')
+def show_login():
+    # generates random anti-forgery state token and
+    # saves it in Flask's session object
+    state = ''.join(random.choice(
+        string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+# page accepting post requests
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps(
+            'Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # obtain authorization code
+    code = request.data
+
+    try:
+        # upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json',
+                                             scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps(
+            'Failed to upgrade the authorization code'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # check that the access token is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           %access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # if there's an error in the access token info, abort
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # verify that the access token is used for the intended user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps(
+            'Token\'s user ID does not match given user ID'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # verify that the access token is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps(
+            'Token\s client ID des not match the app\'s'), 401)
+        print 'Token\s client ID des not match the app\'s'
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # check if user is already logged in
+    # stored_credentials = login_session.get('credentials')
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps(
+            'User is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # store access token in the session for later use
+    #login_session['credentials'] = credentials
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # gets user info
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = {'access_token': credentials.access_token,
+              'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 # API endpoint for a restaurant's menu
 @app.route('/restaurants/<int:restaurant_id>/json/')
@@ -32,17 +235,6 @@ def menu_item_json(restaurant_id, menu_item_id):
                                                   id = menu_item_id).one()
     return jsonify(MenuItem = menu_item.serialize)
 
-# login page
-# will create a state token to prevent request forgery
-# to be stored in the session for later validation
-@app.route('/login/')
-def show_login():
-    # generates random anti-forgery state token and
-    # saves it in Flask's session object
-    state = ''.join(random.choice(
-        string.ascii_uppercase + string.digits) for x in xrange(32))
-    login_session['state'] = state
-    return render_template('login.html')
 
 # list of restaurants
 @app.route('/')
